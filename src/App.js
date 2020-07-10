0318f94c0a4c93b636638094415c5399dfa352e8
bharathjinka09 <bharathjinka09@gmail.com>
2020-07-10 15:46:23 +0530

    Added images

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import requests from './requests'
 function App() {
   return (
     <div className="App">
-    <Row title="NETFLIX ORIGINALS"fetchUrl={requests.fetchNetflixOriginals}/>
+    <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
     <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
       
     </div>
--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from './axios';
+import './Row.css'
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl }){
 	const [movies, setMovies] = useState([]);
@@ -9,18 +12,26 @@ function Row({ title, fetchUrl }){
 		// if [], run once when the row loads, and don't run again
 		async function fetchData(){
 			const request = await axios.get(fetchUrl);
-			console.log(request)
+			setMovies(request.data.results)
 			return request;
 		}
 		fetchData();
-	}, []);
+	}, [fetchUrl]);
+
+	console.log(movies)
 
 	return (
-		<div>
+		<div className="row">
 		   <h2>{title}</h2>
-
+		   
+		   <div className="row__posters">
 		   {/* container -> posters */}
 
+		   {movies.map(movie => (
+                <img className='row__poster' src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+		   	))}
+
+		   </div>
 		</div>
 	)
 }
