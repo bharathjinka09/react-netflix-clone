e22216a1bee94952ea0a974710e6abc62a6fccb1
bharathjinka09 <bharathjinka09@gmail.com>
2020-07-10 17:26:21 +0530

    Added banner

--- src/Row.js
+++ src/Row.js
@@ -4,7 +4,7 @@ import './Row.css'
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl }){
+function Row({ title, fetchUrl, isLargeRow }){
 	const [movies, setMovies] = useState([]);
 
 	// A snippet of code which runs based on a specific condition
@@ -28,7 +28,11 @@ function Row({ title, fetchUrl }){
 		   {/* container -> posters */}
 
 		   {movies.map(movie => (
-                <img className='row__poster' src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                <img
+                 key={movie.id}
+                 className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                 src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
+                 alt={movie.name} />
 		   	))}
 
 		   </div>
