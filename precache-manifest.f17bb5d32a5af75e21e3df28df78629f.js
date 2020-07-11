3a1fd75f472366e08058009970d700a7d09acdd4
bharathjinka09 <bharathjinka09@gmail.com>
2020-07-11 12:48:50 +0530

    Updates

---
+++ precache-manifest.f17bb5d32a5af75e21e3df28df78629f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e695de636943fe55760417febc00103",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "2e82ebc9340649909ebb",
+    "url": "/netflix-clone/static/css/main.d97fa7c8.chunk.css"
+  },
+  {
+    "revision": "d3d14c59bc1192a94339",
+    "url": "/netflix-clone/static/js/2.142d9c3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/netflix-clone/static/js/2.142d9c3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e82ebc9340649909ebb",
+    "url": "/netflix-clone/static/js/main.739fdb38.chunk.js"
+  },
+  {
+    "revision": "ed067e929af6839177a1",
+    "url": "/netflix-clone/static/js/runtime-main.bbcab00e.js"
+  }
+]);
